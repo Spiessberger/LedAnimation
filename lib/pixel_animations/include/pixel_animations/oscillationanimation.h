5ee587c77325f9191690b6931a9951af41910c65
Daniel Spiessberger <80575371+Spiessberger@users.noreply.github.com>
2025-09-27 14:22:32 +0200

    increase amplitude and cycle speed

--- lib/pixel_animations/include/pixel_animations/oscillationanimation.h
+++ lib/pixel_animations/include/pixel_animations/oscillationanimation.h
@@ -8,4 +8,12 @@ public:
   OscillationAnimation(PixelController& controller);
 
   void nextFrame(int frameCount) override;
+
+private:
+  // increase to make cycle animation slower
+  int m_phaseShiftModifier = 100;
+  // increase to cycle through color bands slower
+  int m_cycleSpeedModifier = 5;
+  // increase to get a higher bandwidth of colors
+  int m_amplitude = 70;
 };
--- lib/pixel_animations/oscillationanimation.cpp
+++ lib/pixel_animations/oscillationanimation.cpp
@@ -1,8 +1,6 @@
-#include <pixel_animations/oscillationanimation.h>
-#include <pixel_animations/color.h>
-
 #include <cmath>
-
+#include <pixel_animations/color.h>
+#include <pixel_animations/oscillationanimation.h>
 
 OscillationAnimation::OscillationAnimation(PixelController& controller)
   : Animation(controller)
@@ -11,18 +9,23 @@ OscillationAnimation::OscillationAnimation(PixelController& controller)
 
 void OscillationAnimation::nextFrame(int frameCount)
 {
-  const int maxHue = 360;
-  const double periodFactor = M_PI * 2 / pixelCount();
-  const double phaseShift = M_PI * 2 * frameCount / 100;
 
-  const int minValue = (frameCount / 10) % maxHue;
-  const int amplitude = 60;
+  const int maxHue = 360;
+
+  // map pixelcount to cos period of 2 pi
+  const double periodFactor = M_PI * 2 / pixelCount();
+  // shift phase with to get slightliy new color for each frame
+  const double phaseShift = M_PI * 2 * frameCount / m_phaseShiftModifier;
+
+  // shift base color with ongoing animation to get different colors within the
+  // amplitude
+  const int minValue = (frameCount / m_cycleSpeedModifier) % maxHue;
 
   for (int i = 0; i < pixelCount(); i++)
   {
     const double pixelOffset = periodFactor * i;
-    int hue = std::cos(phaseShift + pixelOffset) * amplitude / 2 +
-              amplitude / 2 + minValue;
+    int hue =
+        (std::cos(phaseShift + pixelOffset) + 1) * m_amplitude / 2 + minValue;
 
     setPixel(i, hsvHueToRGB(hue));
   }
